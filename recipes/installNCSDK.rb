f281d61d920621880fdda5328f42cd563d066f56
Preston Padgett <Preston.Padgett@outlook.com>
2018-01-11 10:49:21 -0600

    Add recipe for MovidiusNCSDK & Kitchen Sysetem Requirements

---
+++ recipes/installNCSDK.rb
@@ -0,0 +1,37 @@
+
+directory '/NeuralComputeStick' do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+directory '/NeuralComputeStick/workspace' do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+git '/NeuralComputeStick/workspace/ncsdk' do
+  repository 'git://github.com/movidius/ncsdk.git'
+  revision 'master'
+  action :sync
+end
+git '/NeuralComputeStick/workspace/ncappzoo' do
+  repository 'git://github.com/movidius/ncappzoo.git'
+  revision 'master'
+  action :sync
+end
+execute 'make install' do
+  cwd '/NeuralComputeStick/workspace/ncsdk'
+  command 'make install'
+  action :run
+end
+execute 'source ~/.bashrc' do
+  command 'source ~/.bashrc'
+  action :run
+end
+execute 'make examples' do
+  cwd '/NeuralComputeStick/workspace/ncsdk'
+  command 'make example'
+  action :run
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -10,9 +10,7 @@ unless os.windows?
   describe user('root'), :skip do
     it { should exist }
   end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+  describe directory('/NeuralComputeStick/workspace') do
+    it { should exist }
+  end
 end
